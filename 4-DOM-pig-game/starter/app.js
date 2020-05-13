a1f70a3c2892404051f268b9694dbc56adf656a1
SeungpilHan <gkstmdvlf2@gmail.com>
2020-05-13 12:44:57 +0900

    game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,20 +11,21 @@ GAME RULES:
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 
-document.querySelector('#score-0').textContent = dice;
+// setter
+document.querySelector('#current-' + activePlayer).textContent = dice;
 
+// getter
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
 
+document.querySelector('.dice').style.display = 'none'
 
-
-
-
-
-
+document.querySelector('.btn-roll').addEventListener('')
 
 
 
